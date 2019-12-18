5175a98fbac7a13be921687298c33fb10e3b9393
transoman <vasiag10@gmail.com>
2019-12-18 11:07:51 +0200

    Ready cta and request sections

--- app/content/themes/irontheme/page-templates/template-home.php
+++ app/content/themes/irontheme/page-templates/template-home.php
@@ -354,6 +354,97 @@ if ( have_rows('home_layout') ):
           </div>
         </section>
       <?php endif; ?>
+
+    <?php elseif ( get_row_layout() == 'cta' ): ?>
+
+      <?php $title = get_sub_field( 'title' );
+      $descr = get_sub_field( 'description' );
+      $btn = get_sub_field( 'button' );
+
+      if ($title): ?>
+        <section class="cta">
+          <div class="container">
+            <div class="row">
+              <div class="cta__content">
+                <h2 class="section-title"><?php echo $title; ?></h2>
+
+                <?php if ($descr): ?>
+                  <p class="cta__descr"><?php echo $descr; ?></p>
+                <?php endif; ?>
+
+                <?php if ($btn['url']): ?>
+                  <a href="<?php echo esc_url( $btn['url'] ); ?>" class="btn"<?php echo $btn['target'] ? ' target="'. $btn['target'] .'"' : '' ;?>><?php echo $btn['title']; ?></a>
+                <?php endif; ?>
+              </div>
+            </div>
+
+            <img src="<?php echo THEME_URL; ?>/images/general/cta-bg.svg" class="cta__bg" alt="">
+            <img src="<?php echo THEME_URL; ?>/images/general/reviews-bg-3.svg" class="cta__bg-2" alt="">
+
+          </div>
+        </section>
+      <?php endif; ?>
+
+    <?php elseif ( get_row_layout() == 'form' ): ?>
+
+      <section class="request">
+        <div class="container">
+
+          <div class="request__wrap">
+            <div class="section-head">
+              <?php $title = get_sub_field( 'title' );
+              $descr = get_sub_field( 'description' );
+
+              if ($title): ?>
+                <h2 class="section-title"><?php echo $title; ?></h2>
+              <?php endif; ?>
+
+              <?php if ($descr): ?>
+                <p class="section-descr"><?php echo $descr; ?></p>
+              <?php endif; ?>
+            </div>
+
+            <div class="request__form">
+              <div id="mc_embed_signup">
+                <form action="" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate">
+                  <div id="mc_embed_signup_scroll">
+                    <div class="request__form-row">
+                      <div class="mc-field-group form-group">
+                        <label for="mce-FNAME" class="form-label">Your full name:</label>
+                        <input type="text" value="" name="FNAME" class="form-field" id="mce-FNAME" placeholder="Enter your name">
+                      </div>
+                      <div class="mc-field-group form-group">
+                        <label for="mce-EMAIL" class="form-label">Your e-mail:<span class="asterisk"></span></label>
+                        <input type="email" value="" name="EMAIL" class="required email form-field" id="mce-EMAIL" placeholder="Enter your email">
+                      </div>
+                      <div class="mc-field-group form-group">
+                        <label for="mce-MMERGE2" class="form-label">Camp name:</label>
+                        <input type="text" value="" name="MMERGE2" class="form-field" id="mce-MMERGE2" placeholder="Enter your camp name">
+                      </div>
+                      <div class="form-group form-group--btn">
+                        <input type="submit" value="send a request" name="subscribe" id="mc-embedded-subscribe" class="btn">
+                      </div>
+                    </div>
+
+                    <div id="mce-responses" class="clear">
+                      <div class="response" id="mce-error-response" style="display:none"></div>
+                      <div class="response" id="mce-success-response" style="display:none"></div>
+                    </div> <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_0d5082c3287b75c366bb2239b_0dc27f002f" tabindex="-1" value=""></div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+
+          <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script>
+          <script type='text/javascript'>(function($){window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='MMERGE2';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+
+          <img src="<?php echo THEME_URL; ?>/images/general/request-bg.svg" class="request__bg" alt="">
+          <img src="<?php echo THEME_URL; ?>/images/general/request-bg-2.svg" class="request__bg-2" alt="">
+
+        </div>
+      </section>
     
     <?php endif;
 
